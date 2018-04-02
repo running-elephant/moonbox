3f57a2899cd6dcb0ab686d3d9bd79bb46e8ea71c
zhikunwu <zhikunwu@creditease.cn>
2018-04-02 05:02:21 -0700

    jdbc server and mb master interface message

--- grid/src/main/scala/moonbox/grid/deploy/rest/entities.scala
+++ grid/src/main/scala/moonbox/grid/deploy/rest/entities.scala
@@ -15,19 +15,19 @@ case class SubmitInbound(token: String, sqls: Seq[String], mode: String) extends
 case class SubmitOutbound(jobId: Option[String] = None,
 	message: Option[String] = None,
 	error: Option[String] = None,
-	schema: Option[Seq[String]] = None, data: Option[Seq[Seq[String]]] = None) extends Outbound
+	schema: Option[String] = None, data: Option[Seq[Seq[Any]]] = None) extends Outbound
 
 case class ProgressInbound(token: String, jobId: String) extends Inbound
 case class ProgressOutbound(jobId: String, error: Option[String] = None, status: Option[String] = None) extends Outbound
 
 case class ResultInbound(token: String, jobId: String, offset: Long, size: Long) extends Inbound
-case class ResultOutbound(jobId: String, error: Option[String] = None, schema: Option[Seq[String]] = None, data: Option[Seq[Seq[String]]] = None) extends Outbound
+case class ResultOutbound(jobId: String, error: Option[String] = None, schema: Option[String] = None, data: Option[Seq[Seq[Any]]] = None) extends Outbound
 
 case class CancelInbound(token: String, jobId: String) extends Inbound
 case class CancelOutbound(jobId: String, error: Option[String] = None) extends Outbound
 
 case class QueryInbound(sessionId: String, token: String, sqls: Seq[String]) extends Inbound
-case class QueryOutbound(error: Option[String] = None, schema: Option[Seq[String]] = None, data: Option[Seq[Seq[String]]] = None) extends Outbound
+case class QueryOutbound(jobId: String, error: Option[String] = None, schema: Option[String] = None, data: Option[Seq[Seq[Any]]] = None, size: Option[Long] = None) extends Outbound
 
 case class OpenSessionInbound(token: String) extends Inbound
 case class OpenSessionOutbound(sessionId: Option[String], error: Option[String]) extends Outbound
