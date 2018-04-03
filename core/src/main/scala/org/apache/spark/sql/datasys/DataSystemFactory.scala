14692ba83e975affadd83d8050b40e763125bca6
wanghao75 <haowang75@creditease.cn>
2018-04-03 17:31:58 +0800

    init

--- core/src/main/scala/org/apache/spark/sql/datasys/DataSystemFactory.scala
+++ core/src/main/scala/org/apache/spark/sql/datasys/DataSystemFactory.scala
@@ -14,6 +14,7 @@ object DataSystemFactory {
 	def typeToSparkDatasource(typ: String): String = {
 		typ match {
 			case "mysql" | "oracle" | "jdbc" => "org.apache.spark.sql.execution.datasources.mbjdbc"
+			case "hbase" => "org.apache.spark.sql.execution.datasources.hbase"
 			case "redis" => "org.apache.spark.sql.execution.datasources.redis"
 			case "mongo" | "mongodb" => "com.mongodb.spark.sql"
 			case "parquet" => "parquet"
