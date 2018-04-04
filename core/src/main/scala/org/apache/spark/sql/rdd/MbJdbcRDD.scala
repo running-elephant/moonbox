aef36b7640485aa4e9966b91740461dfda34cb70
wanghao75 <haowang75@creditease.cn>
2018-04-04 19:12:27 +0800

    init

--- core/src/main/scala/org/apache/spark/sql/rdd/MbJdbcRDD.scala
+++ core/src/main/scala/org/apache/spark/sql/rdd/MbJdbcRDD.scala
@@ -5,6 +5,7 @@ import java.sql.{Connection, ResultSet}
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.{Partition, SparkContext, TaskContext}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.util.NextIterator
 
 import scala.reflect.ClassTag
 
@@ -16,8 +17,47 @@ class MbJdbcRDD[T: ClassTag](sc: SparkContext, getConnection: () => Connection,
 	sql: String,
 	mapRow: ResultSet => T) extends RDD[T](sc, Nil) {
 	@DeveloperApi
-	override def compute(split: Partition, context: TaskContext): Iterator[T] = {
-null
+	override def compute(split: Partition, context: TaskContext): Iterator[T] = new NextIterator[T] {
+
+		context.addTaskCompletionListener( context => closeIfNeeded())
+
+		val conn = getConnection()
+		val statement = conn.createStatement()
+		val resultSet = statement.executeQuery(sql)
+
+		override protected def close(): Unit = {
+			try {
+				if (null != resultSet) {
+					resultSet.close()
+				}
+			} catch {
+				case e: Exception => logWarning("Exception closing resultset", e)
+			}
+			try {
+				if (null != statement) {
+					statement.isClosed
+				}
+			} catch {
+				case e: Exception => logWarning("Exception closing statement", e)
+			}
+			try {
+				if (null != conn) {
+					conn.close()
+				}
+				logInfo("closed connection")
+			} catch {
+				case e: Exception => logWarning("Exception closing connection", e)
+			}
+		}
+
+		override protected def getNext(): T = {
+			if (resultSet != null && resultSet.next()) {
+				mapRow(resultSet)
+			} else {
+				finished = true
+				null.asInstanceOf[T]
+			}
+		}
 	}
 
 	override protected def getPartitions: Array[Partition] = {
