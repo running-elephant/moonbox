02cfeb72b4685756eeae049d2370e24b05d9467d
zhikunwu <zhikunwu@creditease.cn>
2018-07-20 17:28:03 +0800

    update assembly and build script

--- dev/build.sh
+++ dev/build.sh
@@ -4,7 +4,6 @@ current_dir=`pwd`
 script_dir=$(cd `dirname $0`; pwd)
 cd $script_dir
 cd ..
-(mvn clean package -Pdist -Pmysql -Poracle -Psqlserver -Pkudu -Phbase -Pes -Ppresto -Pcassandra -DskipTests)
 ###please add your own extra modules
-###(mvn clean package -Pdist -DskipTests)
+(mvn clean package -Pdist -Pmysql -Poracle -Psqlserver -Pkudu -Phbase -Pes -Ppresto -Pcassandra -Pmongo -DskipTests)
 cd $current_dir
