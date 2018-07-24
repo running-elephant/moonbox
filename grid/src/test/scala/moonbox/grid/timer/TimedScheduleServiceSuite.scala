d96d7f6eb236b4c2bc0c55ec5bfea6bb2944d335
wanghao75 <haowang75@creditease.cn>
2018-07-24 13:04:48 +0800

    notice and license

--- grid/src/test/scala/moonbox/grid/timer/TimedScheduleServiceSuite.scala
+++ grid/src/test/scala/moonbox/grid/timer/TimedScheduleServiceSuite.scala
@@ -1,3 +1,23 @@
+/*-
+ * <<
+ * Moonbox
+ * ==
+ * Copyright (C) 2016 - 2018 EDP
+ * ==
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * >>
+ */
+
 package moonbox.grid.timer
 
 import moonbox.common.MbConf
@@ -16,7 +36,7 @@ class TimedScheduleServiceSuite extends FunSuite with BeforeAndAfterAll {
 		conf.set("moonbox.timer.org.quartz.jobStore.useProperties", "false")
 		conf.set("moonbox.timer.org.quartz.jobStore.tablePrefix", "QRTZ_")
 		conf.set("moonbox.timer.org.quartz.jobStore.dataSource", "quartzDataSource")
-		conf.set("moonbox.timer.org.quartz.dataSource.quartzDataSource.driver", classOf[com.mysql.jdbc.Driver].getName)
+		conf.set("moonbox.timer.org.quartz.dataSource.quartzDataSource.driver", "com.mysql.jdbc.Driver")
 		conf.set("moonbox.timer.org.quartz.dataSource.quartzDataSource.URL", "jdbc:mysql://master:3306/quartz-test")
 		conf.set("moonbox.timer.org.quartz.dataSource.quartzDataSource.user", "root")
 		conf.set("moonbox.timer.org.quartz.dataSource.quartzDataSource.password", "123456")
