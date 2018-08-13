178f9ade078ac18103f338a068c69f5fe122831c
zhikunwu <zhikunwu@creditease.cn>
2018-08-13 15:13:16 +0800

    sbin script change start input param

--- sbin/start-masters.sh
+++ sbin/start-masters.sh
@@ -12,17 +12,16 @@
 # ssh root@B (不需要密码, 登录成功)
 
 #import env firstly
-if [ -z "${MOONBOX_HOME}" ]; then
-    moonbox_home_dir="$(cd "`dirname "$0"`/.."; pwd)"
-    if [ -f "${moonbox_home_dir}/conf/moonbox-env.sh" ]; then
-      . "${moonbox_home_dir}/conf/moonbox-env.sh"
-    fi
-    if [[ $? != 0 ]]; then
-        exit 1
-    fi
+moonbox_home_dir="$(cd "`dirname "$0"`/.."; pwd)"
+if [ -f "${moonbox_home_dir}/conf/moonbox-env.sh" ]; then
+  . "${moonbox_home_dir}/conf/moonbox-env.sh"
 fi
 
-cat "${MOONBOX_HOME}/conf/nodes" | grep "moonbox.grid.master" | grep -v '#' | awk '{print $2}' | cut -d '/' -f 3 | while read line
+
+master_lines=`cat "${MOONBOX_HOME}/conf/nodes" | grep -v '^#' | grep -w "moonbox.grid.master" | awk '{print $2}' | sed 's/grid:\/\///g' | sed 's/[[:space:]]//g'`
+master_url="grid://"`echo $master_lines | sed 's/[[:space:]]/,/g'`
+
+cat "${MOONBOX_HOME}/conf/nodes" | grep "moonbox.grid.master" | grep -v '#' | awk '{print $2}' | cut -d '/' -f 3 | sed 's/[[:space:]]//g'| while read line
 do
     hostname=`echo $line | awk '{print $1}' |cut -d ':' -f 1`
     akka_port=`echo $line | awk '{print $1}' |cut -d ':' -f 2`
@@ -36,14 +35,9 @@ do
     echo "ssh $hostname $ssh_options ..."
 
     ssh -T ${ssh_options} ${hostname}  << EEOF
-        work_home=\${MOONBOX_HOME}
-        if [ -z \$work_home  ]; then
-            echo "remote moonbox_home is empty \${MOONBOX_HOME}, use local MOONBOX_HOME ${MOONBOX_HOME}, ip:${akka_ip}, akka_port:${akka_port}, ssh_options:${ssh_options}"
-            ${MOONBOX_HOME}/sbin/start-master.sh
-        else
-            cd \${work_home}
-            \${work_home}/sbin/start-master.sh
-	fi
+        echo "remote moonbox_home is empty \${MOONBOX_HOME}, use local MOONBOX_HOME ${MOONBOX_HOME}, ip:${akka_ip}, akka_port:${akka_port}, ssh_options:${ssh_options}"
+        ${MOONBOX_HOME}/sbin/start-master.sh  ${hostname}  ${akka_port}  ${master_url}
+
 EEOF
     if [ $? -ne 0 ] ;then
         echo "ERROR: ssh ${hostname} and run command failed, please check"
--- sbin/start-worker.sh
+++ sbin/start-worker.sh
@@ -1,41 +1,27 @@
 #!/bin/env bash
 
+
+#import env firstly
+moonbox_home_dir="$(cd "`dirname "$0"`/.."; pwd)"
+if [ -f "${moonbox_home_dir}/conf/moonbox-env.sh" ]; then
+  . "${moonbox_home_dir}/conf/moonbox-env.sh"
+fi
+
+echo "start worker in $MOONBOX_HOME with param($@)"
+
 local_host=`hostname`
 date=`date +%Y%m%d_%H%M%S`
 
-#import env firstly
-if [ -z "${MOONBOX_HOME}" ]; then
-    moonbox_home_dir="$(cd "`dirname "$0"`/.."; pwd)"
-    if [ -f "${moonbox_home_dir}/conf/moonbox-env.sh" ]; then
-      . "${moonbox_home_dir}/conf/moonbox-env.sh"
-    fi
-    if [[ $? != 0 ]]; then
-        exit 1
-    fi
-fi
-
-echo "start worker in $MOONBOX_HOME ... $#"
-
-
 if [ $# -ne 3 ]; then
 
-    tmp_file="/tmp/master.txt"
-    cat "${MOONBOX_HOME}/conf/nodes" | grep -v '^#' | grep -w "moonbox.grid.master" | awk '{print $2}'  | sed 's/grid:\/\///g'  > ${tmp_file}
-
-    master_url=""
-    while read line
-    do
-        master_url="${master_url},${line}"
-    done < ${tmp_file}
-    master_url="grid://"`echo $master_url | cut -d ',' -f 2-`
-
-    rm -f ${tmp_file}
+    master_lines=`cat "${MOONBOX_HOME}/conf/nodes" | grep -v '^#' | grep -w "moonbox.grid.master" | awk '{print $2}' | sed 's/grid:\/\///g' | sed 's/[[:space:]]//g'`
+    master_url="grid://"`echo $master_lines | sed 's/[[:space:]]/,/g'`
 
     echo "start MbWorker --host ${local_host} --masters ${master_url} --port [random port]"
     java ${WORKER_JAVA_OPTS} -cp ${MOONBOX_HOME}/libs/*:${MOONBOX_HOME}/libs/moonbox-common_2.11-0.2.0-SNAPSHOT.jar:${MOONBOX_HOME}/libs/moonbox-core_2.11-0.2.0-SNAPSHOT.jar:${MOONBOX_HOME}/libs/moonbox-grid_2.11-0.2.0-SNAPSHOT.jar moonbox.grid.deploy.worker.MbWorker --host ${local_host}  --masters ${master_url}  1>${MOONBOX_HOME}/log/"worker-$USER-$date.log" 2>&1 &
 
 else
-    echo "start MbWorker --host ${1} --port ${2} --masters ${3} "
+    echo "start MbWorker(with input) --host ${1} --port ${2} --masters ${3} "
     java ${WORKER_JAVA_OPTS} -cp ${MOONBOX_HOME}/libs/*:${MOONBOX_HOME}/libs/moonbox-common_2.11-0.2.0-SNAPSHOT.jar:${MOONBOX_HOME}/libs/moonbox-core_2.11-0.2.0-SNAPSHOT.jar:${MOONBOX_HOME}/libs/moonbox-grid_2.11-0.2.0-SNAPSHOT.jar moonbox.grid.deploy.worker.MbWorker --host ${1} --port ${2} --masters ${3}  1>${MOONBOX_HOME}/log/"worker-$USER-$date.log" 2>&1 &
 fi
 
