40409fbe5727f0f841724e40de9fcd4ba6415ec1
wanghao75 <haowang75@creditease.cn>
2019-01-07 11:14:55 +0800

    remove comments

--- protocol/src/main/scala/moonbox/protocol/client/protocols.scala
+++ protocol/src/main/scala/moonbox/protocol/client/protocols.scala
@@ -21,17 +21,6 @@ case class LoginOutbound(token: Option[String] = None, error: Option[String] = N
 case class LogoutInbound(token: String) extends Inbound
 case class LogoutOutbound(error: Option[String]) extends Outbound
 
-
-// service
-
-// compute
-
-// interactive mode
-// support local and cluster runtime engine
-// support synchronize mode only
-case class RequestAccessInbound(token: Option[String] = None, isLocal: Boolean) extends Inbound
-case class RequestAccessOutbound(address: Option[String] = None, error: Option[String] = None) extends Outbound
-
 case class OpenSessionInbound(token: String, database: Option[String], config: Map[String, String]) extends Inbound
 case class OpenSessionOutbound(sessionId: Option[String] = None, workerHost: Option[String] = None, workerPort: Option[Int] = None, error: Option[String] = None) extends Outbound
 
@@ -73,59 +62,3 @@ case class BatchQueryProgressOutbound(
 case class BatchQueryCancelInbound(username: String, password: String, jobId: String) extends Inbound
 case class InteractiveQueryCancelInbound(token: String, sessionId: String) extends Inbound
 case class CancelQueryOutbound(error: Option[String] = None) extends Outbound
-
-// job and event
-case class ShowRunningEventsInbound(username: String) extends Inbound
-case class ShowRunningEventsOutbound(error: Option[String] = None,
-								 schema: Option[Seq[String]] = None,
-								 data: Option[Seq[Seq[Any]]] = None) extends Outbound
-
-
-case class ShowNodeJobsInbound(username: String) extends Inbound
-case class ShowNodeJobsOutbound(error: Option[String] = None,
-								 schema: Option[Seq[String]] = None,
-								 data: Option[Seq[Seq[Any]]] = None) extends Outbound
-
-
-case class ShowClusterJobsInbound(username: String) extends Inbound
-case class ShowClusterJobsOutbound(error: Option[String] = None,
-								 schema: Option[Seq[String]] = None,
-								 data: Option[Seq[Seq[Any]]] = None) extends Outbound
-
-
-
-// yarn app
-case class ShowNodesInfoInbound(username: String) extends Inbound
-case class ShowNodesInfoOutbound(error: Option[String] = None,
-								 schema: Option[Seq[String]] = None,
-								 data: Option[Seq[Seq[Any]]] = None) extends Outbound
-
-
-case class AddAppInbound(username: String, config: String) extends Inbound
-case class AddAppOutbound(appId: Option[String] = None,
-						  error: Option[String] = None) extends Outbound
-
-case class RemoveAppInbound(username: String, appId: String) extends Inbound
-case class RemoveAppOutbound(appId: Option[String] = None,
-							 error: Option[String] = None) extends Outbound
-
-case class ShowAppInbound(username: String) extends Inbound
-case class ShowAppOutbound(error: Option[String] = None,
-						   schema: Option[Seq[String]] = None,
-						   data: Option[Seq[Seq[Any]]] = None) extends Outbound
-
-
-// meta
-case class ShowDatabasesInbound(token: String) extends Inbound
-case class DatabaseInfo(name: String, isLogical: Boolean, properties: Map[String, String], description: String)
-case class ShowDatabasesOutbound(error: Option[String] = None,
-								 databases: Option[Seq[DatabaseInfo]] = None) extends Outbound
-
-case class ShowTablesInbound(token: String, database: String = "default") extends Inbound
-case class ShowTablesOutbound(error: Option[String] = None,
-							  tables: Option[Seq[String]] = None) extends Outbound
-
-case class DescribeTablesInbound(token: String, table: String, database: String) extends Inbound
-case class TableInfo(name: String, properties: Map[String, String], columns: Seq[(String, String)], description: String)
-case class DescribeTablesOutbound(error: Option[String] = None,
-								  table: Option[TableInfo] = None) extends Outbound
