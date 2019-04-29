8343f85a37582bccba2c377e9353c74fcc1b8d5e
whtblktone <446787976@qq.com>
2019-04-29 12:19:18 +0800

    add kafka

--- dev/build.sh
+++ dev/build.sh
@@ -5,5 +5,18 @@ script_dir=$(cd `dirname $0`; pwd)
 cd $script_dir
 cd ..
 ###please add your own extra modules
-(mvn clean package -Pdist -Pmysql -Poracle -Psqlserver -Pkudu -Phbase -Pes -Ppresto -Pcassandra -Pmongo -Pclickhouse -DskipTests)
+(
+mvn clean package -Pdist -DskipTests \
+-Pmysql \
+-Poracle \
+-Psqlserver \
+-Pkudu \
+-Phbase \
+-Pes \
+-Ppresto \
+-Pcassandra \
+-Pmongo \
+-Pclickhouse \
+-Pkafka \
+)
 cd $current_dir
