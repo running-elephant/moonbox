fcd4fe4325d00e6876c7f1700605a78ac47590c1
whtblktone <446787976@qq.com>
2019-08-12 11:02:19 +0800

    max rows

--- client/src/main/scala/moonbox/client/ClientOptions.scala
+++ client/src/main/scala/moonbox/client/ClientOptions.scala
@@ -24,88 +24,105 @@ import scala.collection.{immutable, mutable}
 
 class ClientOptions(val options: CaseInsensitiveMap[String]) {
 
-  import ClientOptions._
+	import ClientOptions._
 
-  def this(ops: Map[String, String]) = this(CaseInsensitiveMap(ops))
-  def this() = this(Map.empty[String, String])
+	def this(ops: Map[String, String]) = this(CaseInsensitiveMap(ops))
 
-  private val clientKeys: immutable.Seq[String] = HOST :: PORT :: USER :: PASSWORD :: DATABASE :: READ_TIMEOUT :: FETCH_SIZE :: MAX_ROWS :: SERIALIZER :: Nil
+	def this() = this(Map.empty[String, String])
 
-  val host: String = options.getOrElse(HOST, "localhost")
-  val port: Int = options.get(PORT).map(_.toInt).getOrElse(10010)
-  val database: String = options.getOrElse(DATABASE, "default")
-  val user: Option[String] = options.get(USER)
-  val password: Option[String] = options.get(PASSWORD)
-  val timeout: Int = options.get(READ_TIMEOUT).map(_.toInt).getOrElse(3600) /* time_unit: second */
-  val fetchSize: Int = options.get(FETCH_SIZE).map(_.toInt).getOrElse(1000)
-  val maxRows: Int = options.get(MAX_ROWS).map(_.toInt).getOrElse(Int.MinValue)
-  val isLocal: Boolean = options.get(IS_LOCAL).exists(_.toBoolean)
-  val serializer: String = options.getOrElse(SERIALIZER, "protobuf")
-  val extraOptions: Map[String, String] = options.filter{case (k, _) => !clientKeys.contains(k)}
+	private val clientKeys: immutable.Seq[String] = HOST :: PORT :: USER :: PASSWORD :: DATABASE :: READ_TIMEOUT :: FETCH_SIZE :: MAX_ROWS :: SERIALIZER :: Nil
+
+	val host: String = options.getOrElse(HOST, "localhost")
+	val port: Int = options.get(PORT).map(_.toInt).getOrElse(10010)
+	val database: String = options.getOrElse(DATABASE, "default")
+	val user: Option[String] = options.get(USER)
+	val password: Option[String] = options.get(PASSWORD)
+	val timeout: Int = options.get(READ_TIMEOUT).map(_.toInt).getOrElse(3600)
+	/* time_unit: second */
+	val fetchSize: Int = options.get(FETCH_SIZE).map(_.toInt).getOrElse(1000)
+	val maxRows: Int = options.get(MAX_ROWS).map(_.toInt).getOrElse(Int.MinValue)
+	val isLocal: Boolean = options.get(IS_LOCAL).exists(_.toBoolean)
+	val serializer: String = options.getOrElse(SERIALIZER, "protobuf")
+	val extraOptions: Map[String, String] = options.filter { case (k, _) => !clientKeys.contains(k) }
 }
 
 object ClientOptions {
 
-  val HOST = "host"
-  val PORT = "port"
-  val USER = "user"
-  val PASSWORD = "password"
-  val DATABASE = "database"
-  val READ_TIMEOUT = "timeout"  /* time unit: second */
-  val FETCH_SIZE = "fetchsize"
-  val MAX_ROWS = "maxrows"
-  val IS_LOCAL = "islocal"
-  val SERIALIZER = "serializer"
+	val HOST = "host"
+	val PORT = "port"
+	val USER = "user"
+	val PASSWORD = "password"
+	val DATABASE = "database"
+	val READ_TIMEOUT = "timeout"
+	/* time unit: second */
+	val FETCH_SIZE = "fetchsize"
+	val MAX_ROWS = "maxrows"
+	val IS_LOCAL = "islocal"
+	val SERIALIZER = "serializer"
 
-  def builder() = new Builder()
-  def builder(clientOptions: ClientOptions) = new Builder().options(clientOptions.options)
+	def builder() = new Builder()
 
-  class Builder {
-    private val _options = mutable.Map.empty[String, String]
-    def options(ops: Map[String, String]) = {
-      _options ++= ops
-      this
-    }
-    def host(name: String) = {
-      _options += (HOST -> name)
-      this
-    }
-    def port(value: Int) = {
-      _options += (PORT -> value.toString)
-      this
-    }
-    def user(name: String) = {
-      _options += (USER -> name)
-      this
-    }
-    def password(value: String) = {
-      _options += (PASSWORD -> value)
-      this
-    }
-    def database(name: String) = {
-      _options += (DATABASE -> name)
-      this
-    }
-    def timeout(seconds: Int) = {
-      _options += (READ_TIMEOUT -> seconds.toString)
-      this
-    }
-    def fetchSize(value: Int) = {
-      _options += (FETCH_SIZE -> value.toString)
-      this
-    }
-    def maxRows(value: Long) = {
-      _options += (MAX_ROWS -> value.toString)
-      this
-    }
-    def isLocal(value: Boolean) = {
-      _options += (IS_LOCAL -> value.toString)
-      this
-    }
-    def serializer(name: String) = {
-      _options += (SERIALIZER -> name)
-      this
-    }
-    def build() = new ClientOptions(_options.toMap)
-  }
+	def builder(clientOptions: ClientOptions) = new Builder().options(clientOptions.options)
+
+	class Builder {
+		private val _options = mutable.Map.empty[String, String]
+
+		def options(ops: Map[String, String]) = {
+			_options ++= ops
+			this
+		}
+
+		def host(name: String) = {
+			_options += (HOST -> name)
+			this
+		}
+
+		def port(value: Int) = {
+			_options += (PORT -> value.toString)
+			this
+		}
+
+		def user(name: String) = {
+			_options += (USER -> name)
+			this
+		}
+
+		def password(value: String) = {
+			_options += (PASSWORD -> value)
+			this
+		}
+
+		def database(name: String) = {
+			_options += (DATABASE -> name)
+			this
+		}
+
+		def timeout(seconds: Int) = {
+			_options += (READ_TIMEOUT -> seconds.toString)
+			this
+		}
+
+		def fetchSize(value: Int) = {
+			_options += (FETCH_SIZE -> value.toString)
+			this
+		}
+
+		def maxRows(value: Long) = {
+			_options += (MAX_ROWS -> value.toString)
+			this
+		}
+
+		def isLocal(value: Boolean) = {
+			_options += (IS_LOCAL -> value.toString)
+			this
+		}
+
+		def serializer(name: String) = {
+			_options += (SERIALIZER -> name)
+			this
+		}
+
+		def build() = new ClientOptions(_options.toMap)
+	}
+
 }
--- client/src/main/scala/moonbox/client/MoonboxClient.scala
+++ client/src/main/scala/moonbox/client/MoonboxClient.scala
@@ -26,94 +26,134 @@ import moonbox.client.entity.{ConnectionState, JobState, MoonboxRowSet}
 
 import scala.collection.mutable
 
-object MoonboxClient{
+object MoonboxClient {
 
-  def builder(): Builder = new Builder()
-  def builder(clientOptions: ClientOptions) = {
-    new Builder().setConf(clientOptions.options)
-  }
+	def builder(): Builder = new Builder()
 
-  class Builder {
-    private val settings = mutable.Map.empty[String, String]
-    def setConf(key: String, value: String): Builder = {
-      settings.put(key, value)
-      this
-    }
-    def setConf(kv: Map[String, String]): Builder = {
-      settings ++= kv
-      this
-    }
-    def build(): MoonboxClient = {
-      require(settings.contains(ClientOptions.USER))
-      require(settings.contains(ClientOptions.PASSWORD))
-      new MoonboxClientImpl(settings.toMap)
-    }
-  }
+	def builder(clientOptions: ClientOptions) = {
+		new Builder().setConf(clientOptions.options)
+	}
+
+	class Builder {
+		private val settings = mutable.Map.empty[String, String]
+
+		def setConf(key: String, value: String): Builder = {
+			settings.put(key, value)
+			this
+		}
+
+		def setConf(kv: Map[String, String]): Builder = {
+			settings ++= kv
+			this
+		}
+
+		def build(): MoonboxClient = {
+			require(settings.contains(ClientOptions.USER))
+			require(settings.contains(ClientOptions.PASSWORD))
+			new MoonboxClientImpl(settings.toMap)
+		}
+	}
+
 }
 
 trait MoonboxClient {
 
-  /** constructors */
-  def newClient: MoonboxClient
+	/** constructors */
+	def newClient: MoonboxClient
 
-  /** connection related */
-  def token: String
-  def sessionId: String
-  def version: String
-  def isActive: Boolean
-  def close(): Unit
-  def getReadTimeout: Int  /* time unit: ms */
-  def setReadTimeout(milliseconds: Int): Unit
-  def getFetchSize: Int
-  def setFetchSize(size: Int): Unit
-  def getMaxRows: Int
-  def setMaxRows(size: Int): Unit
-  def getServers: Seq[InetSocketAddress]
-  def getConf(key: String): Option[String]
-  def getAllConf: Map[String, String]
+	/** connection related */
+	def token: String
 
-  /** userSys related */
-  def userInfo: AnyRef
+	def sessionId: String
 
-  /** metadata related */
-  /* list */
-  def listDatabases: Seq[String]
-  def listTables(database: String): Seq[String]
-  def listViews(database: String): Seq[String]
-  def listFunctions(database: String): Seq[String]
-  def listVariables(username: String): Seq[String]
-  /* alter */
-  /* exists */
-  /* create */
-  /* drop */
+	def version: String
 
-  /** interactive query related */
-  def getConnectionState: ConnectionState
-  def getCurrentDatabase: String
-  def setCurrentDatabase(databaseName: String): Unit
-  def interactiveQuery(interactiveSql: Seq[String]): MoonboxRowSet /* query with default fetchSize and timeout */
-  def interactiveQuery(interactiveSql: Seq[String], fetchSize: Int): MoonboxRowSet
-  def interactiveQueryWithTimeout(interactiveSql: Seq[String], milliseconds: Int): MoonboxRowSet
-  /**
-    * @param fetchSize note: ZERO means query with default fetchSize
-    * @param milliseconds note: ZERO means synchronized query without timeout
-    * @return
-    */
-  def interactiveQuery(interactiveSql: Seq[String], fetchSize: Int, milliseconds: Int): MoonboxRowSet
-  def interactiveQuery(interactiveSql: Seq[String], fetchSize: Int, maxRows: Int, milliseconds: Int): MoonboxRowSet
-  def cancelInteractiveQuery(): Boolean
+	def isActive: Boolean
 
-  /** batch query related */
-  def batchQuery(jobSql: Seq[String], config: Map[String, String]): String  /* return jobId */
-  def batchQueryProgress(jobId: String): JobState
-  def cancelBatchQuery(jobId: String): Boolean
-  //  def batchQuery(jobSql: Seq[String], config: String): String  /* return jobId */
-  def runningJobs: Seq[String]
-  def waitingJobs: Seq[String]
-  def failedJobs: Seq[String]
-  def jobHistory: Seq[AnyRef]
+	def close(): Unit
 
-  /** timer related */
-  /** privilege related */
+	def getReadTimeout: Int
+
+	/* time unit: ms */
+	def setReadTimeout(milliseconds: Int): Unit
+
+	def getFetchSize: Int
+
+	def setFetchSize(size: Int): Unit
+
+	def getMaxRows: Int
+
+	def setMaxRows(size: Int): Unit
+
+	def getServers: Seq[InetSocketAddress]
+
+	def getConf(key: String): Option[String]
+
+	def getAllConf: Map[String, String]
+
+	/** userSys related */
+	def userInfo: AnyRef
+
+	/** metadata related */
+	/* list */
+	def listDatabases: Seq[String]
+
+	def listTables(database: String): Seq[String]
+
+	def listViews(database: String): Seq[String]
+
+	def listFunctions(database: String): Seq[String]
+
+	def listVariables(username: String): Seq[String]
+
+	/* alter */
+	/* exists */
+	/* create */
+	/* drop */
+
+	/** interactive query related */
+	def getConnectionState: ConnectionState
+
+	def getCurrentDatabase: String
+
+	def setCurrentDatabase(databaseName: String): Unit
+
+	def interactiveQuery(interactiveSql: Seq[String]): MoonboxRowSet
+
+	/* query with default fetchSize and timeout */
+	def interactiveQuery(interactiveSql: Seq[String], fetchSize: Int): MoonboxRowSet
+
+	def interactiveQueryWithTimeout(interactiveSql: Seq[String], milliseconds: Int): MoonboxRowSet
+
+	/**
+	  * @param fetchSize    note: ZERO means query with default fetchSize
+	  * @param milliseconds note: ZERO means synchronized query without timeout
+	  * @return
+	  */
+	def interactiveQuery(interactiveSql: Seq[String], fetchSize: Int, milliseconds: Int): MoonboxRowSet
+
+	def interactiveQuery(interactiveSql: Seq[String], fetchSize: Int, maxRows: Int, milliseconds: Int): MoonboxRowSet
+
+	def cancelInteractiveQuery(): Boolean
+
+	/** batch query related */
+	def batchQuery(jobSql: Seq[String], config: Map[String, String]): String
+
+	/* return jobId */
+	def batchQueryProgress(jobId: String): JobState
+
+	def cancelBatchQuery(jobId: String): Boolean
+
+	//  def batchQuery(jobSql: Seq[String], config: String): String  /* return jobId */
+	def runningJobs: Seq[String]
+
+	def waitingJobs: Seq[String]
+
+	def failedJobs: Seq[String]
+
+	def jobHistory: Seq[AnyRef]
+
+	/** timer related */
+	/** privilege related */
 
 }
