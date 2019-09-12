a60bf37ef6eb68093ea111f134a729acacd7c0c7
Xiaoyan <1223117836@qq.com>
2019-09-12 16:00:04 +0800

    zeppelin ldap user login

--- external/moonbox-zeppelin-interpreter-0.8/src/main/scala/moonbox/jdbc/interpreter/MoonboxInterpreter.scala
+++ external/moonbox-zeppelin-interpreter-0.8/src/main/scala/moonbox/jdbc/interpreter/MoonboxInterpreter.scala
@@ -84,36 +84,38 @@ class MoonboxInterpreter(property: Properties) extends Interpreter(property) {
     idToConnection.clear()
   }
 
-	private val KEY_SHA = "SHA"
-	private val hexDigits = (0 to 9).++('a' to 'f').map(_.toString)
+  private val KEY_SHA = "SHA"
+  private val hexDigits = (0 to 9).++('a' to 'f').map(_.toString)
 
-	def encryptSHA(data: String): String = {
-		if (data == null || data.equals("")) {
-			""
-		} else {
-			val sha = MessageDigest.getInstance(KEY_SHA)
-			sha.update(data.getBytes)
-			byteArrayToHexString(sha.digest()).toUpperCase
-		}
-	}
+  def encryptSHA(data: String): String = {
+    if (data == null || data.equals("")) {
+      ""
+    } else {
+      val sha = MessageDigest.getInstance(KEY_SHA)
+      sha.update(data.getBytes)
+      byteArrayToHexString(sha.digest()).toUpperCase
+    }
+  }
 
-	private def byteArrayToHexString(bytes: scala.Array[Byte]): String = {
-		bytes.map(byteToHexString).reduce(_ + _)
-	}
+  private def byteArrayToHexString(bytes: scala.Array[Byte]): String = {
+    bytes.map(byteToHexString).reduce(_ + _)
+  }
 
-	private def byteToHexString(byte: Byte): String = {
-		val res = if (byte < 0) byte + 256
-		else byte
-		hexDigits(res / 16) + hexDigits(res % 16)
-	}
+  private def byteToHexString(byte: Byte): String = {
+    val res = if (byte < 0) byte + 256
+    else byte
+    hexDigits(res / 16) + hexDigits(res % 16)
+  }
 
   override def interpret(s: String, interpreterContext: InterpreterContext): InterpreterResult = {
-		val loginUser = interpreterContext.getAuthenticationInfo.getUser
-		val password = encryptSHA(loginUser)
-		val moonboxUser = "bi_hive@" + loginUser
-		baseProps.setProperty("user", moonboxUser)
-		log.info(s"replace login user $loginUser to $moonboxUser")
-		baseProps.setProperty("password", password)
+    val loginUser = interpreterContext.getAuthenticationInfo.getUser
+    if (loginUser.endsWith("@creditease.cn")) {
+      val password = encryptSHA(loginUser.stripSuffix("@creditease.cn"))
+      val moonboxUser = baseProps.getProperty("user").split("@").head + "@" + loginUser.stripSuffix("@creditease.cn")
+      baseProps.setProperty("user", moonboxUser)
+      log.info(s"replace login user $loginUser to $moonboxUser")
+      baseProps.setProperty("password", password)
+    }
     var interpreterResult: InterpreterResult = new InterpreterResult(InterpreterResult.Code.SUCCESS)
     var statement: Statement = null
     var resultSet: ResultSet = null
