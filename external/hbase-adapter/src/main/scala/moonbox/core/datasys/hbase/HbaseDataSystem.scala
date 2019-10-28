c34c79f286048290b4c900e6fea29724ac3f84ff
Xiaoyan <1223117836@qq.com>
2019-10-28 11:09:01 +0800

    hbase test connection add other config

--- external/hbase-adapter/src/main/scala/moonbox/core/datasys/hbase/HbaseDataSystem.scala
+++ external/hbase-adapter/src/main/scala/moonbox/core/datasys/hbase/HbaseDataSystem.scala
@@ -19,46 +19,50 @@
  */
 
 package moonbox.core.datasys.hbase
+
 import moonbox.common.MbLogging
 import moonbox.core.datasys.DataSystem
 import org.apache.hadoop.hbase.client.{Admin, ConnectionFactory, HBaseAdmin}
 import org.apache.hadoop.hbase.{HBaseConfiguration, HConstants}
 
 class HbaseDataSystem(props: Map[String, String]) extends DataSystem(props) with MbLogging {
-	checkOptions("hbase.zookeeper.quorum")
+  checkOptions(HConstants.ZOOKEEPER_QUORUM)
 
-	private def getClient: Admin = {
-		val conf = HBaseConfiguration.create
-		conf.set(HConstants.ZOOKEEPER_QUORUM, props("hbase.zookeeper.quorum"))
+  private def getClient: Admin = {
+    val conf = HBaseConfiguration.create
+    conf.set(HConstants.ZOOKEEPER_QUORUM, props(HConstants.ZOOKEEPER_QUORUM))
+    conf.set(HConstants.ZOOKEEPER_CLIENT_PORT, props.getOrElse(HConstants.ZOOKEEPER_CLIENT_PORT, HConstants.DEFAULT_ZOOKEPER_CLIENT_PORT.toString))
+    conf.set(HConstants.ZOOKEEPER_ZNODE_PARENT, props.getOrElse(HConstants.ZOOKEEPER_ZNODE_PARENT, HConstants.DEFAULT_ZOOKEEPER_ZNODE_PARENT))
+    val connection = ConnectionFactory.createConnection(conf)
+    connection.getAdmin
+  }
 
-		val connection = ConnectionFactory.createConnection(conf)
-		connection.getAdmin
-	}
+  override def tableNames(): Seq[String] = {
+    val admin = getClient
+    val tables: Seq[String] = admin.listTableNames().map(_.getNameAsString)
+    admin.close()
+    tables
+  }
 
-	override def tableNames(): Seq[String] = {
-		val admin = getClient
-		val tables: Seq[String] = admin.listTableNames().map(_.getNameAsString)
-		admin.close()
-		tables
-	}
+  override def tableName(): String = {
+    throw new Exception("Function tableName no implementation, for HBASE does not support physical mount")
+  }
 
-	override def tableName(): String = {
-		throw new Exception("Function tableName no implementation, for HBASE dose not support physical mount")
-	}
+  override def tableProperties(tableName: String): Map[String, String] = {
+    throw new Exception("Function tableProperties no implementation, for HBASE does not support physical mount")
+  }
 
-	override def tableProperties(tableName: String): Map[String, String] = {
-		throw new Exception("Function tableProperties no implementation, for HBASE dose not support physical mount")
-	}
-
-	override def test(): Unit = {
-		try {
-			val conf = HBaseConfiguration.create
-			conf.set(HConstants.ZOOKEEPER_QUORUM, props("hbase.zookeeper.quorum"))
-			HBaseAdmin.checkHBaseAvailable(conf)
-		} catch {
-			case e: Throwable =>
-				logError("hbase test failed", e)
-				throw e
-		}
-	}
+  override def test(): Unit = {
+    try {
+      val conf = HBaseConfiguration.create
+      conf.set(HConstants.ZOOKEEPER_QUORUM, props(HConstants.ZOOKEEPER_QUORUM))
+      conf.set(HConstants.ZOOKEEPER_CLIENT_PORT, props.getOrElse(HConstants.ZOOKEEPER_CLIENT_PORT, HConstants.DEFAULT_ZOOKEPER_CLIENT_PORT.toString))
+      conf.set(HConstants.ZOOKEEPER_ZNODE_PARENT, props.getOrElse(HConstants.ZOOKEEPER_ZNODE_PARENT, HConstants.DEFAULT_ZOOKEEPER_ZNODE_PARENT))
+      HBaseAdmin.checkHBaseAvailable(conf)
+    } catch {
+      case e: Throwable =>
+        logError("hbase test failed", e)
+        throw e
+    }
+  }
 }
