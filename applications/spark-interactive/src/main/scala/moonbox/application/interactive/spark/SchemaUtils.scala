f2caed1ef38ba6ea6d4473a513ad1570ca6bd4e4
whtblktone <446787976@qq.com>
2019-11-20 16:39:02 +0800

    schema

--- applications/spark-interactive/src/main/scala/moonbox/application/interactive/spark/SchemaUtils.scala
+++ applications/spark-interactive/src/main/scala/moonbox/application/interactive/spark/SchemaUtils.scala
@@ -1,10 +1,10 @@
 package moonbox.application.interactive.spark
 
-import moonbox.protocol.protobuf.SchemaPB
+import moonbox.protocol.protobuf.StructTypePB
 import org.apache.spark.sql.types.StructType
 
 object SchemaUtils {
-	def convertSchema(schema: StructType): SchemaPB = {
+	def convertSchema(schema: StructType): StructTypePB = {
 		null
 	}
 
