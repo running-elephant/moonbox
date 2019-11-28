7918edae9a3349afeeb52d87d2a9268db4cfe6f6
whtblktone <446787976@qq.com>
2019-11-28 12:02:35 +0800

    app

--- applications/spark-interactive/src/main/scala/moonbox/application/interactive/spark/SparkServerHandler.scala
+++ applications/spark-interactive/src/main/scala/moonbox/application/interactive/spark/SparkServerHandler.scala
@@ -1,48 +1,5 @@
 package moonbox.application.interactive.spark
 
-import io.netty.buffer.ByteBuf
-import moonbox.network.server.{RpcCallContext, RpcHandler}
-import moonbox.network.util.JavaUtils._
-import moonbox.protocol.protobuf.AppRequestMessage
+class SparkServerHandler {
 
-class SparkServerHandler extends RpcHandler {
-
-	override def receive(request: ByteBuf, context: RpcCallContext): Unit = {
-
-		val req = AppRequestMessage.getDefaultInstance.getParserForType.parseFrom(byteBufToByteArray(request))
-		if (req.hasOpenSession) {
-			handleOpenSession()
-		} else if (req.hasExecute) {
-			handleExecute()
-		} else if (req.hasExecuteResult) {
-			handleExecuteResult()
-		} else if (req.hasExecteCancel) {
-			handleExecuteCancel()
-		} else if (req.hasCloseSession) {
-			handleCloseSession()
-		} else {
-			context.sendFailure(new UnsupportedOperationException("Unsupport app request: " + req))
-		}
-
-	}
-
-	private def handleOpenSession(): Unit = {
-
-	}
-
-	private def handleCloseSession(): Unit = {
-
-	}
-
-	private def handleExecute(): Unit = {
-
-	}
-
-	private def handleExecuteResult(): Unit = {
-
-	}
-
-	private def handleExecuteCancel(): Unit = {
-
-	}
 }
