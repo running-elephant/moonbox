187a91b64e8945d413ba73c021087d2569f056a0
whtblktone <446787976@qq.com>
2019-12-20 20:33:08 +0800

    rename applicaiton file

---
+++ grid/src/main/scala/moonbox/grid/deploy/rest/entities/application.scala
@@ -0,0 +1,35 @@
+package moonbox.grid.deploy.rest.entities
+
+import java.util.Date
+
+case class ApplicationIn(
+	appName: String,
+	appType: String,
+	config: Map[String, String],
+	cluster: Option[String],
+	org: String,
+	worker: Option[String],
+	startOnBoot: Boolean
+)
+
+case class ApplicationOut(
+	org: String,
+	appName: String,
+	appType: String,
+	config: Map[String, String],
+	createTime: Option[Date],
+	startTime: Option[Date],
+	needReload: Option[Boolean],
+	state: Option[String],
+	worker: Option[String],
+	startOnBoot: Boolean
+)
+
+case class ApplicationInfo(
+	name : String,
+	appType: String,
+	startTime: Option[String],
+	state: Option[String],
+	worker: Option[String],
+	exception: Option[String]
+)
