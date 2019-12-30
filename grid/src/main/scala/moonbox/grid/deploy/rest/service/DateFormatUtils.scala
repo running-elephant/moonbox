bd08e2b8ba6ab92914741f854b2442be3482870b
whtblktone <446787976@qq.com>
2019-12-30 10:33:48 +0800

    thread safe date format

--- grid/src/main/scala/moonbox/grid/deploy/rest/service/DateFormatUtils.scala
+++ grid/src/main/scala/moonbox/grid/deploy/rest/service/DateFormatUtils.scala
@@ -1,9 +1,14 @@
 package moonbox.grid.deploy.rest.service
 
-import java.text.SimpleDateFormat
+import java.text.{DateFormat, SimpleDateFormat}
 import java.util.{Date, Locale}
 
 object DateFormatUtils {
-	private val createDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ROOT)
-	def formatDate(date: Date) = createDateFormat.format(date)
+
+	private val localDateFormatter = new ThreadLocal[DateFormat]() {
+		override def initialValue(): DateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ROOT)
+	}
+
+	def formatDate(date: Date) = localDateFormatter.get().format(date)
+
 }
