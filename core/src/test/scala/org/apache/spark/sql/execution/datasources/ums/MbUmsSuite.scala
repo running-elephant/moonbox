96250966aae8c2d204b56b4ede246a8392423ab0
Xiaoyan <1223117836@qq.com>
2020-03-03 10:59:38 +0800

    remove unused test

--- core/src/test/scala/org/apache/spark/sql/execution/datasources/ums/MbUmsSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/execution/datasources/ums/MbUmsSuite.scala
@@ -118,43 +118,4 @@ test("ums") {
     )
     session.sql("select * from a").show()
   }
-
-  test("ums") {
-    session.sql(
-      """create table a(
-        			  |ums_id_ long,
-        			  |ums_ts_ timestamp,
-        			  |ums_op_ string,
-        			  |ums_uid_ string,
-        			  |ID long,
-        			  |ACC_TYPE_ID string,
-        			  |USER_ID long,
-        			  |ACC_ID string
-        			  |) using ums options(path '/tmp/user_account_new.txt', allowNumericLeadingZeros 'true')
-      			""".stripMargin
-    )
-    session.sql("select count(*) from a").show(1000000)
-  }
-
-  test("ums1") {
-    session.sql(
-      """
-        |create table a(
-        |ums_id_ long,ums_ts_ timestamp,ums_op_ string,ums_uid_ long,calendar_name string,description string,end_time long,job_data string,job_group string,job_name string,misfire_instr int,next_fire_time long,prev_fire_time long,priority int,sched_name string,start_time long,trigger_group string,trigger_name string,trigger_state string,trigger_type string
-        |) using ums options(path 'file:///Users/swallow/Desktop/data.txt', allowNumericLeadingZeros 'true', mode 'FAILFAST')
-""".stripMargin
-    )
-    session.sql("select calendar_name from\na where calendar_name is null").show()
-  }
-
-  // allowNumericLeadingZeros 'true',
-
-  test("json") {
-    val str = "{\"payload\":[{\"tuple\":[\"349910095380811\",\"2019-10-23 23:33:45.156616\",\"u\",\"60730989463\",\"53801231\",\"2\",\"1\",\"Y20161215000030888\",\"4800.00349921\",\"4800.00349921\",\"4800.00349921\",\"0.0\",\"0.0\",\"0.0\",\"0.0\",\"创建账户\",\"2\",\"0\",\"2016-12-15 13:48:29\",\"2019-10-23 23:34:38\"]},{\"tuple\":[\"349910095383891\",\"2019-10-23 23:33:45.156616\",\"u\",\"60730989464\",\"202436124\",\"4\",\"1\",\"Y20161215000030888\",\"5000.0\",\"5000.0\",\"5000.0\",\"0.0\",\"0.0\",\"0.0\",\"0.0\",\"创建账户\",\"4\",\"0\",\"2019-10-23 23:34:38\",\"2019-10-23 23:34:39\"]},{\"tuple\":[\"349910095385503\",\"2019-10-23 23:33:45.156616\",\"u\",\"60730989465\",\"53801231\",\"2\",\"1\",\"Y20161215000030888\",\"0.00349921\",\"0.00349921\",\"0.00349921\",\"0.0\",\"0.0\",\"0.0\",\"0.0\",\"创建账户\",\"2\",\"0\",\"2016-12-15 13:48:29\",\"2019-10-23 23:34:39\"]},{\"tuple\":[\"349910095386610\",\"2019-10-23 23:33:45.156616\",\"u\",\"60730989466\",\"5254136\",\"1\",\"1\",\"YRD1503000155827\",\"-3.4981265498E8\",\"-3.4981265498E8\",\"-3.4981265498E8\",\"0.0\",\"0.0\",\"0.0\",\"0.0\",\"创建账户\",\"1\",\"1\",\"2015-03-19 10:49:14\",\"2019-10-23 23:34:39\"]},{\"tuple\":[\"349910095387804\",\"2019-10-23 23:33:47.162839\",\"i\",\"60730989467\",\"202436125\",\"8\",\"1\",\"YRD1507000463486\",\"0.0\",\"0.0\",\"0.0\",\"0.0\",\"0.0\",\"0.0\",\"0.0\",\"创建账户\",\"8\",\"0\",\"2019-10-23 23:34:41\",\"2019-10-23 23:34:41\"]}],\"protocol\":{\"type\":\"data_increment_data\",\"version\":\"1.3\"},\"schema\":{\"batchId\":0,\"fields\":[{\"encoded\":false,\"name\":\"ums_id_\",\"nullable\":false,\"type\":\"long\"},{\"encoded\":false,\"name\":\"ums_ts_\",\"nullable\":false,\"type\":\"datetime\"},{\"encoded\":false,\"name\":\"ums_op_\",\"nullable\":false,\"type\":\"string\"},{\"encoded\":false,\"name\":\"ums_uid_\",\"nullable\":false,\"type\":\"string\"},{\"encoded\":false,\"name\":\"ACC_ID\",\"nullable\":false,\"type\":\"long\"},{\"encoded\":false,\"name\":\"TYPE_ID\",\"nullable\":true,\"type\":\"long\"},{\"encoded\":false,\"name\":\"SYS_ID\",\"nullable\":true,\"type\":\"long\"},{\"encoded\":false,\"name\":\"USER_PASSPORTID\",\"nullable\":true,\"type\":\"string\"},{\"encoded\":false,\"name\":\"TOTAL_VALUE\",\"nullable\":true,\"type\":\"decimal\"},{\"encoded\":false,\"name\":\"MONEY_VALUE\",\"nullable\":true,\"type\":\"decimal\"},{\"encoded\":false,\"name\":\"UNLOCK_VALUE\",\"nullable\":true,\"type\":\"decimal\"},{\"encoded\":false,\"name\":\"LOCK_VALUE\",\"nullable\":true,\"type\":\"decimal\"},{\"encoded\":false,\"name\":\"FREEZE_VALUE\",\"nullable\":true,\"type\":\"decimal\"},{\"encoded\":false,\"name\":\"TRANSIT_VALUE\",\"nullable\":true,\"type\":\"decimal\"},{\"encoded\":false,\"name\":\"TRANSIT_LOCK_VALUE\",\"nullable\":true,\"type\":\"decimal\"},{\"encoded\":false,\"name\":\"ACC_DESC\",\"nullable\":true,\"type\":\"string\"},{\"encoded\":false,\"name\":\"TYPE_CODE\",\"nullable\":true,\"type\":\"string\"},{\"encoded\":false,\"name\":\"ACC_STATUS\",\"nullable\":true,\"type\":\"string\"},{\"encoded\":false,\"name\":\"CREATION_TIME\",\"nullable\":true,\"type\":\"datetime\"},{\"encoded\":false,\"name\":\"UPDATED_TIME\",\"nullable\":true,\"type\":\"datetime\"}],\"namespace\":\"oracle.acc3_new.ACC.USER_ACCOUNT.0.0.0\"}"
-    val mapper = new ObjectMapper()
-
-    val ums = mapper.readTree(str)
-
-  }
-
 }
